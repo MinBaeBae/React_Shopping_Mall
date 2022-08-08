e4668e64ee339485a315ce8791e8bafcb19ec8e3
MinBaeBae <msbae92@gmail.com>
2022-08-08 19:59:08 +0900

    [FEAT]- Created Menu Tab

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,10 +1,12 @@
-import { React } from 'react'
+import { React, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import styled from 'styled-components'
+import { Nav } from 'react-bootstrap'
+import { TabContent } from '../component'
 
 const Detail = (props) => {
 	let { id } = useParams()
 	let newShoes = props.shoes.find((x) => x.id == id)
+	let [tab, setTab] = useState(0)
 
 	return (
 		<>
@@ -26,6 +28,39 @@ const Detail = (props) => {
 						<button className="btn btn-danger">주문하기</button>
 					</div>
 				</div>
+				<Nav variant="tabs" defaultActiveKey="link0">
+					<Nav.Item>
+						<Nav.Link
+							eventKey="link0"
+							onClick={() => {
+								setTab(0)
+							}}
+						>
+							버튼0
+						</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link
+							eventKey="link1"
+							onClick={() => {
+								setTab(1)
+							}}
+						>
+							버튼1
+						</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link
+							eventKey="link2"
+							onClick={() => {
+								setTab(2)
+							}}
+						>
+							버튼2
+						</Nav.Link>
+					</Nav.Item>
+				</Nav>
+				<TabContent tab={tab} />
 			</div>
 		</>
 	)
